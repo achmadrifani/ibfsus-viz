08b388754b6466b43e25f8833a73eaa578b560ac
Rifani <achmad.rifani.93@gmail.com>
2024-05-01 15:13:18 +0700

    modify css

--- monitoring_ibf_app.py
+++ monitoring_ibf_app.py
@@ -4,6 +4,7 @@ import folium
 import geopandas as gpd
 import pandas as pd
 from datetime import datetime
+from streamlit_extras.stylable_container import stylable_container
 
 st.set_page_config(layout="wide", page_title="SUS - IBF Visualization Platform", page_icon="🌧️")
 DATA_DIR = "data"
@@ -61,17 +62,96 @@ def prevf():
 # def main():
 st.title("SUS - IBF Visualization Platform")
 data = load_latest_data()
-st.write(f"### Latest data update: {data['sent'].max().strftime('%d %b %Y %H:%M') } UTC")
+with stylable_container(
+    key="data-update",
+    css_styles=["""
+    {position: absolute;
+    top: 680px;
+    left: 30px;
+    z-index: 5;}""",]):
+    st.write(f"Latest data update: {data['sent'].max().strftime('%d %b %Y %H:%M') } UTC")
 timelist = sorted(data["effective"].unique())
 
-valid_date = st.select_slider("Select Time", options=timelist, key="slider", format_func=lambda x: x.strftime("%d %b"))
+with stylable_container(
+    key="select-time",
+    css_styles=["""
+    {background-color: #f4f4f4;
+    padding: 10px;
+    position: absolute;
+    top:710px;
+    width: 100%;
+    z-index:5;
+    box-sizing:none;}""",]):
+    valid_date = st.select_slider("Select Time", options=timelist, key="slider", format_func=lambda x: x.strftime("%d %b"))
 
 cols = st.columns([.1,.1,.8],gap='small')
-with cols[0]:
+with stylable_container(
+    key="button-prev",
+    css_styles=["""
+    button {
+        background-color: #f4f4f4;
+        color: #333;
+        border: 1px solid #333;
+        padding: 5px;
+        margin: 5px;
+        position: absolute;
+        left: 20px;
+        top: 490px;
+        z-index: 5;}""",
+        """
+    button:hover {
+        background-color: #ff8300;
+        color: #f4f4f4;
+        border: 1px solid #333;
+        border-radius: 50px;
+        padding: 5px;
+        margin: 5px;}"""]):
     st.button("Previous", on_click=prevf, key="prev")
-with cols[1]:
+
+with stylable_container(
+        key="button-next",
+        css_styles=["""
+    button {
+        background-color: #f4f4f4;
+        color: #333;
+        border: 1px solid #333;
+        padding: 5px;
+        margin: 5px;
+        position: absolute;
+        left: 100px;
+        top: 475px;
+        z-index: 5;}""",
+        """
+    button:hover {
+        background-color: #ff8300;
+        color: #f4f4f4;
+        border: 1px solid #333;
+        border-radius: 50px;
+        padding: 5px;
+        margin: 5px;}"""]):
     st.button("Next", on_click=nextf, key="next")
-with cols[2]:
+
+with stylable_container(
+        key="button-refresh",
+        css_styles=["""
+    button {
+        background-color: #f4f4f4;
+        color: #333;
+        border: 1px solid #333;
+        padding: 5px;
+        margin: 5px;
+        position: absolute;
+        left: 20px;
+        top: 410px;
+        z-index: 5;}""",
+        """
+    button:hover {
+        background-color: #ff8300;
+        color: #f4f4f4;
+        border: 1px solid #333;
+        border-radius: 50px;
+        padding: 5px;
+        margin: 5px;}"""]):
     st.button("Refresh Data", on_click=load_latest_data, key="refresh")
 
 if valid_date.day not in st.session_state:
@@ -87,7 +167,14 @@ folium.plugins.Fullscreen(
     title_cancel="Exit me",
     force_separate_button=True,
 ).add_to(m)
-st_folium(m,feature_group_to_add=[st.session_state[f"map_{valid_date.day}"]],use_container_width=True)
+with stylable_container(
+    key="map_container",
+    css_styles=["""
+    {position: absolute;
+    top:100px;
+    z-index:1;}""",]
+):
+    st_folium(m,feature_group_to_add=[st.session_state[f"map_{valid_date.day}"]],use_container_width=True, key="map_container")
 
 
 # if __name__ == "__main__":
